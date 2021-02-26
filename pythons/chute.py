74f35dca045e00277de1523d3ec6284846c2d1ae
Joris deguet <joris.deguet@gmail.com>
2021-02-26 11:49:18 -0500

    scattered points for speed based on 2 points

--- pythons/chute.py
+++ pythons/chute.py
@@ -18,6 +18,12 @@ print(heights)
 x = np.array(times)
 y = np.array(heights)
 
+speeds = [0]
+for i in range(1, len(x)):
+    speed = (y[i] - y [i-1]) / (x[i] - x[i-1]) /10.0
+    speeds.append(speed)
+print(speeds)
+
 # calcul une approx polynomial de y en x^i i allant jusqu'à 3
 deg3 = np.polyfit(x, y, 1)
 p3 = np.poly1d(deg3)       # créé une série affichable sur le plot
@@ -33,6 +39,7 @@ acc = np.poly1d(accel)
 
 xp = np.linspace(0, 5, 300)
 
+plt.plot(x, speeds, '.')
 plt.plot(x, y, '.')
 plt.plot(x, p4(x), '-', x, vitesse(x), '--', x, accel(x), '--')
 plt.show()
