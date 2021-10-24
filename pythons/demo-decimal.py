4eca3f55ef24b1579f5d21c0d1b03d20401cf1b9
deguet <joris.deguet@gmail.com>
2021-10-24 07:24:24 -0400

    Types pour manip exact de nombres décimaux

--- pythons/demo-decimal.py
+++ pythons/demo-decimal.py
@@ -1,14 +1,24 @@
 from decimal import Decimal
 
+
 point1 = Decimal(0.1)
 point3 = Decimal(0.3)
 print(point1)
 print(point3)
 print(point1+point1+point1 == point3)
-
 # quoi?
+
+
 point1 = Decimal("0.1")
 point3 = Decimal("0.3")
+print(point1)
+print(point3)
+print(point1+point1+point1 == point3)
+# as-tu une explication?
+
+
+point1 = Decimal(1) / 10
+point3 = Decimal(3) / 10
 print(point1)
 print(point3)
 print(point1+point1+point1 == point3)
