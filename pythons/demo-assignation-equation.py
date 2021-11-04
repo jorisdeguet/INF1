ac41d09b21fba85957a3929ebfdbbc826b9c430f
deguet <joris.deguet@gmail.com>
2021-11-04 06:00:26 -0400

    Pourquoi = n'est pas une équation

--- pythons/demo-assignation-equation.py
+++ pythons/demo-assignation-equation.py
@@ -2,6 +2,12 @@
 
 x = 5
 
-y = 4*x*x + 12              # ici combien vaut y
+y = 4*x*x + 12
+# ici combien vaut y :::                y vaut ____
 
-x = 1                       # et maintenant combien vaut y
+x = 1
+# avec x changé combien vaut y          y vaut ____
+print(y)
+
+y = 4*x*x + 12
+# on se répète non?                     y vaut ____
