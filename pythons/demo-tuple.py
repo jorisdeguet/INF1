cf2ffd2f18e62c692b35d3d05309df459ad8a905
Joris Deguet <joris.deguet@gmail.com>
2022-11-30 20:01:50 -0500

    Exemple assignation dans un tuple

--- pythons/demo-tuple.py
+++ pythons/demo-tuple.py
@@ -1,4 +1,4 @@
-
+print("------------------------------------------")
 # un tuple c'est plusieurs valeurs mises ensemble par forcément du même type
 a = (4, 5, " test")
 print(a)
@@ -6,3 +6,14 @@ print(a[0]) # le premier element du tuple
 print(a[1])
 print(a[2])
 print(len(a)) # le nombre d'elements dans le tuple
+
+print("------------------------------------------")
+# prend le premier morceau de a le met dans x, le 2eme dans y et le 3eme dans y
+(x, y, y) = a
+print(x)
+print(y)
+
+print("------------------------------------------")
+(x, y, z) = a
+print(x)
+print(y)
